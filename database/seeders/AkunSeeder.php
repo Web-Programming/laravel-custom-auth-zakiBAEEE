16619671573f3608f808a93a8554d1a99d70a2ff
zakiBAEEE <138183562+zakiBAEEE@users.noreply.github.com>
2023-11-22 12:14:21 +0700

    ADD : run function in AkunSeeder

--- database/seeders/AkunSeeder.php
+++ database/seeders/AkunSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class AkunSeeder extends Seeder
 {
@@ -13,5 +15,34 @@ class AkunSeeder extends Seeder
     public function run(): void
     {
         //
+        $user = [
+            [
+                'username' => 'admin',
+                'name' => 'AkunAdmin',
+                'email' => 'admin@gmail.com',
+                'level' => 'admin',
+                'password' => Hash::make('123456')
+            ],
+
+            [
+                'username' => 'user1',
+                'name' => 'AkunUser1',
+                'email' => 'user1@gmail.com',
+                'level' => 'user',
+                'password' => Hash::make('123456')
+            ],
+            [
+                'username' => 'user2',
+                'name' => 'AkunUser2',
+                'email' => 'user2@gmail.com',
+                'level' => 'user',
+                'password' => Hash::make('123456')
+            ],
+
+        ];
+
+        foreach ($user as $key => $value) {
+            User::create($value);
+        }
     }
 }
